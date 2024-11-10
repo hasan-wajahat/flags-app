481fb940cde2a248c42ad53d4f06d83da22dbd32
hasan <hasan_wajahat@hotmail.com>
2024-11-10 15:37:05 +0500

    Improves initial page styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import clsx from "clsx";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={clsx(geistSans.variable, geistMono.variable, "antialiased bg-green-100")}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Flags Game</h1>
+    <main className="text-blue-500 text-center">
+      <h1 className="text-4xl font-extrabold mb-6">Flags Game</h1>
 
-      <ul>
+      <ul className="flex justify-center space-x-4">
         <li>
-          <button>Easy</button>
+          <button className="bg-yellow-400 text-white font-bold py-2 px-4 rounded-full hover:bg-yellow-500 transition duration-300">
+            Easy
+          </button>
         </li>
         <li>
-          <button>Medium</button>
+          <button className="bg-green-400 text-white font-bold py-2 px-4 rounded-full hover:bg-green-500 transition duration-300">
+            Medium
+          </button>
         </li>
         <li>
-          <button>Hard</button>
+          <button className="bg-red-400 text-white font-bold py-2 px-4 rounded-full hover:bg-red-500 transition duration-300">
+            Hard
+          </button>
         </li>
       </ul>
     </main>
