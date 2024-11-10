69631a0529ab4f2678b0fd123f3af016e11edbfb
hasan <hasan_wajahat@hotmail.com>
2024-11-10 17:06:12 +0500

    Adds hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main className="text-blue-500 text-center">
-      <h1 className="text-4xl font-extrabold mb-6">Flags Game</h1>
+    <main className="text-center text-blue-500">
+      <div className="mb-8 mt-20 flex justify-center">
+        <Image
+          src="/bluey-dancing.gif"
+          alt="Bluey Dancing"
+          width={200}
+          height={200}
+        />
+      </div>
+      <h1 className="mb-6 text-4xl font-extrabold">Flags Game</h1>
 
       <ul className="flex justify-center space-x-4">
         <li>
-          <button className="bg-yellow-400 text-white font-bold py-2 px-4 rounded-full hover:bg-yellow-500 transition duration-300">
+          <button className="rounded-full bg-yellow-400 px-4 py-2 font-bold text-white transition duration-300 hover:bg-yellow-500">
             Easy
           </button>
         </li>
         <li>
-          <button className="bg-green-400 text-white font-bold py-2 px-4 rounded-full hover:bg-green-500 transition duration-300">
+          <button className="rounded-full bg-green-400 px-4 py-2 font-bold text-white transition duration-300 hover:bg-green-500">
             Medium
           </button>
         </li>
         <li>
-          <button className="bg-red-400 text-white font-bold py-2 px-4 rounded-full hover:bg-red-500 transition duration-300">
+          <button className="rounded-full bg-red-400 px-4 py-2 font-bold text-white transition duration-300 hover:bg-red-500">
             Hard
           </button>
         </li>
